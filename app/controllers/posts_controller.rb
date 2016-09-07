a0a30375f76c166d3b2c34efce2aecb7c926221c
raymond <ckwingngai@gmail.com>
2016-09-07 15:04:01 +0800

    bug fixed

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+  def index
+    @content_first = "this is the first contents"
+    @content_second = "this is the second contents"
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
